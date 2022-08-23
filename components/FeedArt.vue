32a598a133a95a41cbd46f14fbb4f89856353335
Alex Kozack <cawa-93@users.noreply.github.com>
2022-08-23 13:52:04 +0300

    feat: Спроба оптимізувати зображення в фіді

--- components/FeedArt.vue
+++ components/FeedArt.vue
@@ -1,20 +1,47 @@
 <script lang="ts" setup>
-import { ElButton, ElCard } from 'element-plus';
+import {ElCard} from 'element-plus';
 import TaxonomyList from "~/components/TaxonomyList.vue";
+import {computed, useRequestHeaders} from "#imports";
 
-defineProps<{
+const props = defineProps<{
   fandom?: string[]
   pairing?: string[]
   tags?: string[]
   art: string
 }>();
+
+const optimizedPreview = computed(() => {
+  const host = process.client ? location.host : useRequestHeaders().host
+  const absoluteUrl = props.art.startsWith('http') ? props.art : host ? `http${host.startsWith('localhost') ? '' : 's'}://${host}${props.art}` : props.art
+
+  try {
+    const parsedUrl = new URL(absoluteUrl)
+
+    if (parsedUrl.hostname === 'localhost') {
+      return props.art
+    }
+
+    parsedUrl.pathname = parsedUrl.host+parsedUrl.pathname
+    parsedUrl.port = ''
+    parsedUrl.host = 'i0.wp.com'
+    parsedUrl.protocol = 'https:'
+    parsedUrl.searchParams.set('w', '320')
+    parsedUrl.searchParams.set('quality','50')
+
+    return parsedUrl.toString()
+  } catch (_) {
+    console.warn(_)
+    return props.art
+  }
+})
+
 </script>
 
 <template>
   <nuxt-link class="link-card" :to="art" target="_blank">
     <el-card :body-style="{ padding: '0px' }">
       <img
-        :src="art"
+        :src="optimizedPreview"
         class="image"
       />
       <taxonomy-list style="padding: 14px"
--- components/FeedComics.vue
+++ components/FeedComics.vue
@@ -1,19 +1,47 @@
 <script lang="ts" setup>
 import { ElCard } from 'element-plus';
+import {computed, useRequestHeaders} from "#imports";
 
-defineProps<{
+const props = defineProps<{
   fandom?: string[]
   pairing?: string[]
   tags?: string[]
   comics: string[]
   _path: string
 }>();
+
+
+const optimizedPreview = computed(() => {
+  const host = process.client ? location.host : useRequestHeaders().host
+  const art = props.comics[0]
+  const absoluteUrl = art.startsWith('http') ? art : host ? `http${host.startsWith('localhost') ? '' : 's'}://${host}${art}` : art
+
+  try {
+    const parsedUrl = new URL(absoluteUrl)
+
+    if (parsedUrl.hostname === 'localhost') {
+      return art
+    }
+
+    parsedUrl.pathname = parsedUrl.host+parsedUrl.pathname
+    parsedUrl.port = ''
+    parsedUrl.host = 'i0.wp.com'
+    parsedUrl.protocol = 'https:'
+    parsedUrl.searchParams.set('w', '320')
+    parsedUrl.searchParams.set('quality','50')
+
+    return parsedUrl.toString()
+  } catch (_) {
+    console.warn(_)
+    return art
+  }
+})
 </script>
 
 <template>
   <nuxt-link class="link-card" :to="_path">
     <el-card :body-style="{ padding: '0px' }">
-      <img :src="comics[0]" alt=""/>
+      <img :src="optimizedPreview" alt=""/>
 
       <taxonomy-list style="padding: 14px"
                      :fandom="fandom"
