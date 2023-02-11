7be2abcb6d380e434f90081b74b558b3afc6f81b
Alex Kozack <cawa-93@users.noreply.github.com>
2023-02-11 12:57:08 +0200

    Основний макет

--- layouts/Base.vue
+++ layouts/Base.vue
@@ -1,26 +1,58 @@
 <script lang="ts" setup>
-import {provide, ref} from "#imports";
-import {ID_INJECTION_KEY} from 'element-plus';
-import {IconMenu} from '@iconify-prerendered/vue-ep'
-
-
-provide(ID_INJECTION_KEY, {
-  prefix: 100,
-  current: 0,
-});
-
+import {IconRoundArrowUpward} from '@iconify-prerendered/vue-ic'
 const mobileOpen = ref(false);
 </script>
 <template>
   <div class="base-root">
 
-    <div class="backdrop" :class="{mobileOpen}" @click="mobileOpen = !mobileOpen"></div>
+    <button class="mobile-toggle fixed bottom-0 bg-light-50" @click="mobileOpen = !mobileOpen">
+      <IconRoundArrowUpward class="transition-transform duration-300" :class="{'transform rotate-180': mobileOpen}"/>
+    </button>
+
+
     <div class="site-nav" :class="{mobileOpen}">
-      <button class="mobile-toggle" @click="mobileOpen = !mobileOpen">
-        <IconMenu class="mobile-toggle-icon"/>
-      </button>
-      <site-menu class="menu"/>
+      <div class="bg-light-50 py-4">
+        START
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet animi dignissimos dolores ducimus earum facere, iure laboriosam libero magnam magni nesciunt optio qui, quia repellat repellendus repudiandae totam voluptate voluptatum?
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet animi dignissimos dolores ducimus earum facere, iure laboriosam libero magnam magni nesciunt optio qui, quia repellat repellendus repudiandae totam voluptate voluptatum?
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet animi dignissimos dolores ducimus earum facere, iure laboriosam libero magnam magni nesciunt optio qui, quia repellat repellendus repudiandae totam voluptate voluptatum?
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet animi dignissimos dolores ducimus earum facere, iure laboriosam libero magnam magni nesciunt optio qui, quia repellat repellendus repudiandae totam voluptate voluptatum?
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet animi dignissimos dolores ducimus earum facere, iure laboriosam libero magnam magni nesciunt optio qui, quia repellat repellendus repudiandae totam voluptate voluptatum?
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet animi dignissimos dolores ducimus earum facere, iure laboriosam libero magnam magni nesciunt optio qui, quia repellat repellendus repudiandae totam voluptate voluptatum?
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet animi dignissimos dolores ducimus earum facere, iure laboriosam libero magnam magni nesciunt optio qui, quia repellat repellendus repudiandae totam voluptate voluptatum?
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet animi dignissimos dolores ducimus earum facere, iure laboriosam libero magnam magni nesciunt optio qui, quia repellat repellendus repudiandae totam voluptate voluptatum?
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet animi dignissimos dolores ducimus earum facere, iure laboriosam libero magnam magni nesciunt optio qui, quia repellat repellendus repudiandae totam voluptate voluptatum?
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet animi dignissimos dolores ducimus earum facere, iure laboriosam libero magnam magni nesciunt optio qui, quia repellat repellendus repudiandae totam voluptate voluptatum?
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet animi dignissimos dolores ducimus earum facere, iure laboriosam libero magnam magni nesciunt optio qui, quia repellat repellendus repudiandae totam voluptate voluptatum?
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet animi dignissimos dolores ducimus earum facere, iure laboriosam libero magnam magni nesciunt optio qui, quia repellat repellendus repudiandae totam voluptate voluptatum?
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet animi dignissimos dolores ducimus earum facere, iure laboriosam libero magnam magni nesciunt optio qui, quia repellat repellendus repudiandae totam voluptate voluptatum?
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet animi dignissimos dolores ducimus earum facere, iure laboriosam libero magnam magni nesciunt optio qui, quia repellat repellendus repudiandae totam voluptate voluptatum?
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet animi dignissimos dolores ducimus earum facere, iure laboriosam libero magnam magni nesciunt optio qui, quia repellat repellendus repudiandae totam voluptate voluptatum?
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet animi dignissimos dolores ducimus earum facere, iure laboriosam libero magnam magni nesciunt optio qui, quia repellat repellendus repudiandae totam voluptate voluptatum?
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet animi dignissimos dolores ducimus earum facere, iure laboriosam libero magnam magni nesciunt optio qui, quia repellat repellendus repudiandae totam voluptate voluptatum?
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet animi dignissimos dolores ducimus earum facere, iure laboriosam libero magnam magni nesciunt optio qui, quia repellat repellendus repudiandae totam voluptate voluptatum?
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet animi dignissimos dolores ducimus earum facere, iure laboriosam libero magnam magni nesciunt optio qui, quia repellat repellendus repudiandae totam voluptate voluptatum?
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet animi dignissimos dolores ducimus earum facere, iure laboriosam libero magnam magni nesciunt optio qui, quia repellat repellendus repudiandae totam voluptate voluptatum?
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet animi dignissimos dolores ducimus earum facere, iure laboriosam libero magnam magni nesciunt optio qui, quia repellat repellendus repudiandae totam voluptate voluptatum?
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet animi dignissimos dolores ducimus earum facere, iure laboriosam libero magnam magni nesciunt optio qui, quia repellat repellendus repudiandae totam voluptate voluptatum?
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet animi dignissimos dolores ducimus earum facere, iure laboriosam libero magnam magni nesciunt optio qui, quia repellat repellendus repudiandae totam voluptate voluptatum?
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet animi dignissimos dolores ducimus earum facere, iure laboriosam libero magnam magni nesciunt optio qui, quia repellat repellendus repudiandae totam voluptate voluptatum?
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet animi dignissimos dolores ducimus earum facere, iure laboriosam libero magnam magni nesciunt optio qui, quia repellat repellendus repudiandae totam voluptate voluptatum?
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet animi dignissimos dolores ducimus earum facere, iure laboriosam libero magnam magni nesciunt optio qui, quia repellat repellendus repudiandae totam voluptate voluptatum?
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet animi dignissimos dolores ducimus earum facere, iure laboriosam libero magnam magni nesciunt optio qui, quia repellat repellendus repudiandae totam voluptate voluptatum?
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet animi dignissimos dolores ducimus earum facere, iure laboriosam libero magnam magni nesciunt optio qui, quia repellat repellendus repudiandae totam voluptate voluptatum?
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet animi dignissimos dolores ducimus earum facere, iure laboriosam libero magnam magni nesciunt optio qui, quia repellat repellendus repudiandae totam voluptate voluptatum?
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet animi dignissimos dolores ducimus earum facere, iure laboriosam libero magnam magni nesciunt optio qui, quia repellat repellendus repudiandae totam voluptate voluptatum?
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet animi dignissimos dolores ducimus earum facere, iure laboriosam libero magnam magni nesciunt optio qui, quia repellat repellendus repudiandae totam voluptate voluptatum?
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet animi dignissimos dolores ducimus earum facere, iure laboriosam libero magnam magni nesciunt optio qui, quia repellat repellendus repudiandae totam voluptate voluptatum?
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet animi dignissimos dolores ducimus earum facere, iure laboriosam libero magnam magni nesciunt optio qui, quia repellat repellendus repudiandae totam voluptate voluptatum?
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet animi dignissimos dolores ducimus earum facere, iure laboriosam libero magnam magni nesciunt optio qui, quia repellat repellendus repudiandae totam voluptate voluptatum?
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet animi dignissimos dolores ducimus earum facere, iure laboriosam libero magnam magni nesciunt optio qui, quia repellat repellendus repudiandae totam voluptate voluptatum?
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet animi dignissimos dolores ducimus earum facere, iure laboriosam libero magnam magni nesciunt optio qui, quia repellat repellendus repudiandae totam voluptate voluptatum?
+        END
+      </div>
     </div>
+
 
     <div class="content-wrapper">
       <slot/>
@@ -43,7 +75,17 @@ const mobileOpen = ref(false);
 .site-nav {
   width: 100%;
   max-width: 250px;
-  background: white;
+  height: 100vh;
+  overflow-y: auto;
+  scrollbar-width: thin;
+}
+
+.mobile-toggle {
+  width: 100%;
+  height: var(--toggle-nav-height);
+  border: none;
+  font-size: 1.3rem;
+  z-index: 10;
 }
 
 /* MOBILE */
@@ -54,51 +96,20 @@ const mobileOpen = ref(false);
     padding-block-end: var(--toggle-nav-height);
   }
 
-  .mobile-toggle {
-    width: 100%;
-    height: var(--toggle-nav-height);
-    border: none;
-    background: none;
-    font-size: 1.3rem;
-    cursor: pointer;
-  }
-
-  .mobile-toggle-icon {
-    height: 50%;
-    width: auto;
-    opacity: 0.5;
-  }
-
   .site-nav {
     width: 100%;
     position: fixed;
     max-width: 100%;
     border-top: 1px solid var(--el-menu-border-color);
-    bottom: 0;
+    bottom: var(--toggle-nav-height);
+    height: calc(100vh - var(--toggle-nav-height));
     transition: transform var(--animation-duration);
     z-index: 5;
+    padding-block-start: calc(60vh - var(--toggle-nav-height));
   }
 
   .site-nav:not(.mobileOpen) {
-    transform: translateY(calc(100% - var(--toggle-nav-height)))
-  }
-
-  .backdrop {
-    position: fixed;
-    top: 0;
-    left: 0;
-    width: 100vw;
-    height: 100vh;
-    background: black;
-    opacity: 0;
-    z-index: 4;
-    pointer-events: none;
-    transition: opacity var(--animation-duration);
-  }
-
-  .backdrop.mobileOpen {
-    pointer-events: auto;
-    opacity: 0.5;
+    transform: translateY(100%)
   }
 }
 
